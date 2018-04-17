30399648779ea6fb8ca0948ab292f61d55e98b42
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-04-17 11:25:37 -0700

    fixed scrap boy

--- Assets/RMTest.cs
+++ Assets/RMTest.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RMTest : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class RMTest : BaseSingletonBehaviour<RMTest>
+{
+    ResourceManager resMan;
+    // Use this for initialization
+    // Update is called once per frame
+    void Start()
+    {
+        resMan = GameObject.FindObjectOfType<ResourceManager>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            resMan.GrainManager(4);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            resMan.ScrapManager(3);
+        }
+    }
 }
