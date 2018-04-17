78f98590a1c3fd0fea7466d8fe2123de4036dbd8
Dominic Lagle <dominic.lagle444@gmail.com>
2018-04-17 15:22:25 -0700

    Changes to the UI sounds great to me Real Time Pause is working. Insane!!!!!

--- Assets/Dominic/D_Scripts/HomeBaseHealthBehaviour.cs
+++ Assets/Dominic/D_Scripts/HomeBaseHealthBehaviour.cs
@@ -35,10 +35,10 @@ public class HomeBaseHealthBehaviour : MonoBehaviour {
     {
         if (other.gameObject.tag == "regEnemyBullet")
         {
-            health -= 10;
+            health -= 3;
         }
         if(other.gameObject.tag == "Bomb"){
-            health -= 25;
+            health -= 17;
         }
     }
 
