1ec8cd788e1ea2695f63b445764dfc73e5d8f9c1
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-04-19 11:07:48 -0700

    addded Efrans stuff

---
+++ Assets/DDDj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDDj : MonoBehaviour
+{
+    public AudioClip[] theMusics;
+
+    void Update()
+    {
+        if (Input.GetKey("1"))
+        {
+            int clipPick = Random.Range(0, theMusics.Length);
+            GetComponent<AudioSource>().clip = theMusics[clipPick];
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
