3a1d5b9a4c37fdd059d0750250a4a74c1f9f0d3b
GALAXYCANNON\Matthew Casillano <MatthewAC@outlook.com>
2018-04-26 15:14:55 -0700

    Animations and controllers.

---
+++ Assets/Matthew/NewBullets+Trails/SpawnBulletTrail.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBulletTrail : MonoBehaviour {
+
+    public GameObject toot;
+    public GameObject offset;
+
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("SpawnToot" , 0f, 0.02f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //SpawnToot();
+	}
+
+    void SpawnToot() {
+        //Instantiate(toot, new Vector2 (transform.position.x, transform.position.y - .01f),Quaternion.identity);
+        GameObject bullet = Instantiate(toot, toot.transform.position + offset.transform.position, transform.rotation) as GameObject;
+    }
+}
