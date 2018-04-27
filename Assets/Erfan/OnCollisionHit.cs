3312d2e429a71f82e733aabd7b2dca5cef44545b
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-04-27 11:13:21 -0700

    Push up cannon wip and erfan wip scdript

---
+++ Assets/Erfan/OnCollisionHit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitSound : MonoBehaviour
+{
+    public AudioClip[] clips;
+    public void IGotHitSound()
+    {
+        int clipPick = Random.Range(0, clips.Length);
+        GetComponent<AudioSource>().clip = clips[clipPick];
+        GetComponent<AudioSource>().Play();
+    }
+}
