90d7bc1ed9374ef34bb2ac495eaa6a5b08ff9e0a
HenryCGitHub <Henryyoungchoi@gmail.com>
2018-04-29 15:56:48 -0700

    adjusted button animation speed

---
+++ Assets/AnimationSpeed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationSpeed : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+
+	void Start () 
+	{
+		gameObject.GetComponent<Animator>().speed = speed;
+	}
+}
